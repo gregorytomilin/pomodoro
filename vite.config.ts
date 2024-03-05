371da771e708be098ec397b68cc0e5e040149371
Gregory Tomilin <gregory.tomilin@gmail.com>
2024-03-05 10:10:25 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,21 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
